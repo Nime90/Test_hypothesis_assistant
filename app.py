e52789d25c76002daf888cfd7e5871e849dadaef
NIME90 <nicola.menale@lenus.io>
2024-12-09 15:58:41 +0100

    testing headers

--- app.py
+++ app.py
@@ -39,7 +39,9 @@ def save_log(log_info):
 st.title("Hypothesis Testing Assistant")
 #email = get_authenticated_user_email()
 st.write(st.context.headers.get('X-Streamlit-User'))
-st.write(st.experimental_user)
+st.write(st.experimental_user.email)
+st.write(st.context)
+
 #st.write(f"Greetings, {email} 👋")
 
 uploaded_file = st.file_uploader("Choose an Excel file", type=["xlsx", "xls"])
